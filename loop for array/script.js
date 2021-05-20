8b5c85684a473e9d0d145041bc93633b509ebe5d
Rick Mota <ricardorickricardosmt@gmail.com>
2021-05-20 18:37:40 -0300

    adicionei os arquivos e pastas

---
+++ loop for array/script.js
@@ -0,0 +1,6 @@
+let carros = ['corsa', 'palio', 'monza','celta' ];
+
+let html = '<ul>';
+for(let c in carros){
+
+}
---
+++ loop for/script.js
@@ -0,0 +1,7 @@
+let texto = '';
+
+for(let i = 0; i <= 50; i++){
+    texto = texto + i +'<br>';
+}
+
+document.getElementById("demo").innerHTML = texto;
