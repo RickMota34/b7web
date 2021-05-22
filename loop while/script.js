29f4320c2916478b7fd877336b01b05584dcea7e
Rick Mota <ricardorickricardosmt@gmail.com>
2021-05-21 21:48:17 -0300

    comentei o for e while

--- loop while/script.js
+++ loop while/script.js
@@ -1,11 +1,11 @@
 let html = '';
 
 let c = 0;
-while(c < 10){
+while(c < 10){ //while
     html +="Numero: "+c+"<br>";
     c++;
 }
-for(c = 0; c < 10; c++){
+for(c = 0; c < 10; c++){ //for
     html += "numero: "+c+ "<br>";
 }
 document.getElementById("demo").innerHTML = html;
