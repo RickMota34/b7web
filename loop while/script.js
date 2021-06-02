5a68054de5dfa9d75092c85fdcdce812f391dd25
Rick Mota <ricardorickricardosmt@gmail.com>
2021-06-02 14:59:25 -0300

    adicionei algumas linhas de codigo

--- loop while/script.js
+++ loop while/script.js
@@ -1,11 +1,9 @@
 let html = '';
 
 let c = 0;
-while(c < 10){ //while
-    html +="Numero: "+c+"<br>";
+
+while( c < 10){
+    html += 'Número: '+c+'<br>';
     c++;
-}
-for(c = 0; c < 10; c++){ //for
-    html += "numero: "+c+ "<br>";
 }
 document.getElementById("demo").innerHTML = html;
---
+++ querySelector/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuerySelector/QuerySelectorAll</title>
+</head>
+<body>
+    <h3>Ingredientes do bolo</h3>
+    <ul>
+       <li>massa</li>
+       <li>ovo</li>
+       <li>fermento</li>
+       <li>acucar</li>
+    </ul>
+    <h3>Modo preparo</h3>
+    <ul>
+       <li>faca XYZ, Depois WY</li>
+    </ul>
+</body>
+</html>
